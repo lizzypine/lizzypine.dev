5f7622a5a4c4af9b8df5a3c2e4392227aabb08cc
Lizzy <elizabethpine4@gmail.com>
2023-07-13 14:06:25 -1000

    Add leaf animation to header

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,6 +2,9 @@
 import { motion } from "framer-motion"
 import { AiFillGithub } from "react-icons/ai"
 import { AiFillLinkedin } from "react-icons/ai"
+import Image from 'next/image'
+import palmLeaf from "../../public/palm-leaf.png"
+import montserrat from "../../public/montserrat.png"
 
 export default function Header() {
   const line1 = "Lizzy Pine"
@@ -47,7 +50,27 @@ export default function Header() {
 
   return(
     <header className="banner h-screen flex items-center justify-center">
-        <div className="text-center">
+      <motion.div className="flex flex-col items-center "
+        initial={{ y: -90 }}
+        animate={{ y: 0, transition: { delay: 1.5, duration: 1 } }}
+      >
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { delay: 1.5, duration: 1.5 } }}
+        >
+            <Image 
+              src={montserrat} 
+              alt="Montserrat leaf"
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                width: '55%',
+                height: 'auto',
+                rotate: '30deg',
+                translate: '40% -15%',
+              }}
+            />
+          </motion.div>
           <motion.h1 
             className="text-4xl tracking-tight sm:text-6xl load-screen--message"
             variants={sentence}
@@ -77,35 +100,35 @@ export default function Header() {
                 )
               })} 
           </motion.p>
-          <div className="mt-6 flex items-center justify-center gap-x-6">
-            <motion.div
-              initial={{ x: -100, opacity: 0 }}
-              animate={{ x: 0, opacity: 1, transition: { delay: 1.8 } }}
-              whileHover={{
-                scale: 1.2,
-                transition: 0.2
-              }}
-              whileTap={{ scale: 0.8 }}
-            >
-              <a href="https://github.com/lizzypine" className="social-link" target="_blank" rel="noopener noreferrer">
-                <AiFillGithub size="2.5em" />
-              </a>
-            </motion.div>
-            <motion.div
-              initial={{ x: 200, opacity: 0 }}
-              animate={{ x: 0, opacity: 1, transition: { delay: 1.8 } }}
-              whileHover={{
-                scale: 1.2,
-                transition: 0.2
-              }}
-              whileTap={{ scale: 0.8 }}
-            >
-              <a href="https://www.linkedin.com/in/lizzypine/" className="social-link" target="_blank" rel="noopener noreferrer">
-                <AiFillLinkedin size="2.5em" />
-              </a>
-            </motion.div>
-          </div>
+         <div className="mt-6 flex items-center justify-center gap-x-6">
+          <motion.div
+            initial={{ x: -500, opacity: 0 }}
+            animate={{ x: 0, opacity: 1, transition: { delay: 2.3 } }}
+            whileHover={{
+              scale: 1.2,
+              transition: 0.2
+            }}
+            whileTap={{ scale: 0.8 }}
+          >
+            <a href="https://github.com/lizzypine" className="social-link" target="_blank" rel="noopener noreferrer">
+              <AiFillGithub size="2.5em" />
+            </a>
+          </motion.div>
+          <motion.div
+            initial={{ x: 200, opacity: 0 }}
+            animate={{ x: 0, opacity: 1, transition: { delay: 2.3 } }}
+            whileHover={{
+              scale: 1.2,
+              transition: 0.2
+            }}
+            whileTap={{ scale: 0.8 }}
+          >
+            <a href="https://www.linkedin.com/in/lizzypine/" className="social-link" target="_blank" rel="noopener noreferrer">
+              <AiFillLinkedin size="2.5em" />
+            </a>
+          </motion.div>
         </div>
-      </header>
+      </motion.div>
+    </header>
   );
 }
