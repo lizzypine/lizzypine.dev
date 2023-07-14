5e4cb09af841935432c9d91273b69c629319af94
Lizzy <elizabethpine4@gmail.com>
2023-07-13 14:38:06 -1000

    Update footer with illustration citation

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,8 +3,17 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return(
-    <footer className="font-light text-center m-3">
-      © {currentYear} Lizzy Pine
+    <footer className="font-light text-center m-5">
+      <hr class="border-gray-500 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <div className="grid grid-cols-2 gap-6">
+        <div className="">
+        © {currentYear} Lizzy Pine
+        </div>
+        <div className="">
+          Illustrations made by Krit Kongjundee from <a href="https://vecteezy.com" target="_blank" rel="noopener noreferrer" className="underline">vecteezy.com</a> 
+        </div>
+      </div>
+      
       </footer>
   );
 }
